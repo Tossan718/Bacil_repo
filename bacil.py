21c03eb5126df65b052ea0e98aba79e8d9fe3363
Tossan Aditya <tossan.aditya@gmail.com>
2024-03-18 07:13:07 +0700

    18 Maret

---
+++ bacil.py
@@ -0,0 +1,15 @@
+
+import math 
+
+radius_str = input ("10")
+radius_int = int (radius_str)
+
+circumference = 2 * math.pi * radius_int 
+area = math.pi * (radius_int ** 2)
+
+print ("the cirumference is:",circumference, \)
+
+my_var = 15
+print ("my var has a value of: ',myVar")>>
+>>>> 0 
+
