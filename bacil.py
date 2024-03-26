d3a1083a6af9a1bb80ea0472ff89868cdff6dbec
Tossan Aditya <tossan.aditya@gmail.com>
2024-03-26 15:58:57 +0700

    shortened version

--- bacil.py
+++ bacil.py
@@ -1,14 +1,4 @@
-
-import math 
-
-radius_str = input ()
-radius_int = int (radius_str)
-
-circumference = 2 * math.pi * radius_int 
-area = math.pi * (radius_int ** 2)
-
-print ("the cirumference is:",circumference,)
-
-my_var = ()
-print ('my var has a value of: ,' my_var )>>>>>> 0 
+num1 = int(input('Please enter an integer value: '))
+num2 = int(input('Please enter another integer value: '))
+print(num1, '+', num2, '=', num1 + num2)
 
