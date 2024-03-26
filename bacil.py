b4465db2e1525083ccf43b8ca4dc37120f73bafb
Tossan Aditya <tossan.aditya@gmail.com>
2024-03-26 15:12:11 +0700

    coba input

--- bacil.py
+++ bacil.py
@@ -1,7 +1,7 @@
 
 import math 
 
-radius_str = input ("10")
+radius_str = input ()
 radius_int = int (radius_str)
 
 circumference = 2 * math.pi * radius_int 
@@ -9,7 +9,6 @@ area = math.pi * (radius_int ** 2)
 
 print ("the cirumference is:",circumference,)
 
-my_var = 15
-print ("my var has a value of: ',myVar")>>
->>>> 0 
+my_var = ()
+print ('my var has a value of: ,' my_var )>>>>>> 0 
 
