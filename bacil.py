74186367d06b1fb20029ba0ca8597309d5a8c381
Tossan Aditya <tossan.aditya@gmail.com>
2024-03-30 16:26:26 +0700

    basic arithmetic

--- bacil.py
+++ bacil.py
@@ -1,6 +1,6 @@
-w, x, y, z = 12, 50, 45, 9
-print(w, x, y, z)
-print(w, x, y, z, sep=',')
-print(w, x, y, z, sep='')
-print(w, x, y, z, sep=':')
-print(w, x, y, z, sep='-----')
+num1 = eval(input('Enter the First number: '))
+num2 = eval(input('Enter the Second number: '))
+
+sum = num1 / num2 
+print(num1, '/', num2, '=', sum)
+
--- belajar python.py
+++ belajar python.py
@@ -1,4 +1,5 @@
 num1 = eval(input('Enter the First number: '))
 num2 = eval(input('Enter the Second number: '))
-sum = num1 + num2
-print(num1, '+', num2, '=', sum)
+sum = num1 // num2
+print(num1, '//', num2, '=', sum)
+
