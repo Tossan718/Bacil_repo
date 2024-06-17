c8ae967fe6f2b1f1de985acf2c8ea4a961756f02
Tossan Aditya <tossan.aditya@gmail.com>
2024-06-17 18:39:30 +0700

    file baru

--- bacil.py
+++ bacil.py
@@ -2,13 +2,13 @@ print('## Program Python Piramida ##')
 
 print()
  
-tinggi_piramida = int (input('berapa banyak :'))
+tinggi_piramida = int (input('berapa baris :'))
 print()
 
 for i in range(tinggi_piramida):
   for j in range(tinggi_piramida-i):
     print(' ',end='')
      
-  for k in range(i+1):
+  for j in range(i+1):
     print('$ ',end='')
   print()
--- belajar python.py
+++ belajar python.py
@@ -1,10 +1,13 @@
 # belajar triangle 
-n = 5
+n = 5       
 for i in range (n): 
 
-    for j in range (i+1) : 
+    for j in range (i,n) : 
         print (' ', end=' ')
 
-    for j in range (i,n):
-        print ('*', end=' ')
-    print ()
+    for j in range (i):
+        print ('#', end=' ')
+    
+    for k in range (i+1):
+        print ('#', end=' ')
+    print ()  
