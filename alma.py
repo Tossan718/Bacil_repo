a53759fd10f1d6799689dc27f1bdc5177d81a75f
Tossan Aditya <tossan.aditya@gmail.com>
2024-06-20 16:42:26 +0700

    final version

--- alma.py
+++ alma.py
@@ -1,9 +1,11 @@
 def bottle_verse () :
-   print ("bottles of vodka on the wall")
+   
    print ("Take one down, pass it around")
 
 bottle_verse()
 
 for n in range (99, 0, -1) :
-   print ((n), 'bottles of vodka on the wall' )
+   print ('99 bottles of vodka on the wall')
    bottle_verse()
+   print ((n), 'bottles of vodka on the wall' )
+   
