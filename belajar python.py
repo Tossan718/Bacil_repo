4d277517c93eada47b7a7b28d021dcfc371864cc
Tossan Aditya <tossan.aditya@gmail.com>
2024-07-09 17:08:28 +0700

    dikit

--- belajar python.py
+++ belajar python.py
@@ -1,6 +1,17 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    if x == "cherry" : 
-       break 
-    print (x) 
-    
+import turtle
+import math 
+
+antares= turtle.Turtle ()
+antares.screen.bgcolor("pink")
+
+antares.color("blue")
+
+def antares = turtle.Turtle (): 
+   antares.forward(200)
+   antares.right(150)
+
+
+
+
+turtle.done()
+
--- jupyturtle.py
+++ jupyturtle.py
@@ -1,14 +1,30 @@
 import turtle 
-def kotak (): 
-    alma = turtle.Turtle()
+alma = turtle.Turtle()
 
-    forward = 120
-    left = 90 
-    for i in range (4): 
-        alma.forward (forward)
-        alma.left (left)
-   
 
-kotak ()
+# alma.forward (100)
+# alma.left (90)
+# alma.forward (100)
+# alma.left (90)
+# alma.forward (100)
+# alma.left (90)
+# alma.forward (100)
 
+
+alma.begin_fill()
+alma.color ("blue")
+
+alma.forward (150)
+alma.right (90)
+alma.forward (150)
+alma.right (90)
+alma.forward (150)
+alma.right (90)
+alma.forward (150)
+
+alma.penup()
+alma.forward (100)
+alma.pendown () 
+alma.end_fill ()
+turtle.Turtle ()
 turtle .done ()
